987bc47e3468be8fcadd93543f6458c8415b8543
Mohammed Shekha <msh@openerp.com>
2021-01-07 19:56:38 +0530

    [IMP] Improve package.json and tsconfig for autoreload server and watch mode and update index.html file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+// TO add dev-dependency in package.json we have to install npm package with --save-dev option
+// e.g. npm install --save-dev package_name
+
+// TODO: MSH: How to add JS file and use it as a library
+(async () => {
+    // await owl.whenReady();
+    await new Promise(function (resolve) {
+            if (document.readyState !== "loading") {
+                resolve();
+            } else {
+                document.addEventListener("DOMContentLoaded", resolve, false);
+            }
+        });
+    (document.querySelector("span") as HTMLElement).innerText = "Hello World! Hi";
+})();
